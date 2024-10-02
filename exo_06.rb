5483517e0aa6b5e1b042b377bf47498fe517d700
Noravgz <Vergoznora@gmail.com>
2024-10-02 11:29:56 +0200

    changement

--- exo_06.rb
+++ exo_06.rb
@@ -1,6 +1,6 @@
 puts "Bonjour peux tu me donner un nombre ?"
 nombre = gets.chomp.to_i
-nombre.times do
+(nombre - 1).times do
     puts "Bonjour, toi!"
 end
 # va mettre autant de fois "Bonjour, toi" que le nombre choisi
